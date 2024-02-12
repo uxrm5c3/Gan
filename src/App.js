d64ae024be2e6afbd3099249324c8bd5dd168f61
uxrm5c3 <135967678+uxrm5c3@users.noreply.github.com>
2024-02-12 20:20:19 +0530

    Level 1 complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './header';
+import Nav from './nav';
+import Main from './main';
+import Footer from './footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      Homepage
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <img src="/images/Logo.svg" alt="littlelemon logo" />
+        </header>
+    );
+}
+
+export default Header;
